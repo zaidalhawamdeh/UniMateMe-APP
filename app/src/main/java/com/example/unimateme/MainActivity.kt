6049767d9183481efcb47e947b538fdcb254845c
zaidalhawamdeh <afaqstore9@gmail.com>
2025-08-21 14:46:35 +0400

    Update MainActivity.kt

--- app/src/main/java/com/example/unimateme/MainActivity.kt
+++ app/src/main/java/com/example/unimateme/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.example.unimateme
 
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Create a WebView programmatically
-        val webView = WebView(this)
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.webViewClient = WebViewClient()
-        // Load the HTML file from the assets folder
-        webView.loadUrl("file:///android_asset/student_updated.html")
-        setContentView(webView)
+        setContentView(R.layout.activity_main)
     }
 }
